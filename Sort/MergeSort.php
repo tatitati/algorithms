0afe281bb9e4ef76dd992d009e3f929c0bbdb7c5
Francisco Albusac <francisco.albusac@bbc.co.uk>
2018-08-28 00:16:00 +0100

    Added mergesort

---
+++ Sort/MergeSort.php
@@ -0,0 +1,103 @@
+<?php
+
+$data = [9, 500, 2, 30, 1, 80, 123];
+
+
+function splitLeftAndRight($list)
+{
+    $midX = floor(count($list)/2);
+
+    return [
+        array_slice($list, 0, $midX),
+        array_slice($list, $midX),
+    ];
+}
+
+function merge($left, $right)
+{
+    $mergedList = [];
+
+    while(count($left) + count($right) > 0) {
+        if (count($left) == 0) {
+            $mergedList[] = $right[0];
+            array_shift($right);
+            continue;
+        }
+
+        if (count($right) == 0) {
+            $mergedList[] = $left[0];
+            array_shift($left);
+            continue;
+        }
+
+        if ($left[0] < $right[0]) {
+            $mergedList[] = $left[0];
+            array_shift($left);
+            continue;
+        }
+
+        $mergedList[] = $right[0];
+        array_shift($right);
+    }
+
+    return $mergedList;
+}
+
+function mergeSort($list)
+{
+    if (count($list) < 2) {
+        return $list;
+    }
+
+    list($left, $right) = splitLeftAndRight($list);
+    return merge(mergeSort($left), mergeSort($right));
+}
+
+/**
+ * @test
+ */
+$listA = [9, 500, 2, 30, 1];
+list($left, $right) = splitLeftAndRight($listA);
+assert($left === [9, 500]);
+assert($right === [2, 30, 1]);
+
+/**
+ * @test
+ */
+$listA = [9, 500, 2, 30];
+list($left, $right) = splitLeftAndRight($listA);
+assert($left === [9, 500]);
+assert($right === [2, 30]);
+
+/**
+ * @test
+ */
+$listA = [9];
+$listB = [];
+assert(merge($listA, $listB) == [9]);
+
+/**
+ * @test
+ */
+$listA = [100];
+$listB = [8];
+assert(merge($listA, $listB) == [8, 100]);
+
+/**
+ * @test
+ */
+$listA = [3, 100];
+$listB = [8];
+assert(merge($listA, $listB) == [3, 8, 100]);
+
+/**
+ * @test
+ */
+$listA = [3, 100, 8];
+assert(mergeSort($listA) == [3, 8, 100]);
+
+/**
+ * @test
+ */
+$listA = [3, 100, 8, 15, 500];
+assert(mergeSort($listA) == [3, 8, 15, 100, 500]);
