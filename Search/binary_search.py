480d93476f6c42a867cef0e0d25a006dc558633a
tatitati <francis.albusac@simplybusiness.co.uk>
2021-05-02 23:27:19 +0100

    more

---
+++ Search/binary_search.py
@@ -0,0 +1,35 @@
+def binary_search_recursive(arr, low, high, x):
+    if high >= low:
+        mid = (high + low) // 2
+
+        if arr[mid] == x:
+            return mid
+        elif arr[mid] > x:
+            return binary_search_recursive(arr, low, mid - 1, x)
+        else:
+            return binary_search_recursive(arr, mid + 1, high, x)
+
+    else:
+        return -1
+
+def binary_search_iterative(arr, x):
+    low = 0
+    high = len(arr) - 1
+    mid = 0
+
+    while low <= high:
+        mid = (high + low) // 2
+
+        if arr[mid] < x:
+            low = mid + 1
+        elif arr[mid] > x:
+            high = mid - 1
+        else:
+            return mid
+
+    return -1
+
+
+arr = [ 2, 3, 4, 10, 40 ]
+print(binary_search_recursive(arr, 0, len(arr)-1, 10)) # element present at index: 3
+print(binary_search_iterative(arr, 10)) # element present at index: 3
