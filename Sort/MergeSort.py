33999ac4d7320f0c5967ead81075bfabb6b73d6f
tatitati <francis.albusac@simplybusiness.co.uk>
2021-05-03 20:54:14 +0100

    more

--- Sort/MergeSort.py
+++ Sort/MergeSort.py
@@ -1,3 +1,12 @@
+# This algorithm is one of the most efficient techniques to sort an array and it is implemented using the divide and conquer approach.
+
+# - An initial array is divided into two roughly equal parts. If the array has an odd number of elements, one of those "halves" is by
+# one element larger than the other.
+# - The subarrays are divided over and over again into halves until you end up with arrays that have only one element each.
+# - Then you combine the pairs of one-element arrays into two-element arrays, sorting them in the process. Then these sorted pairs
+# are merged into four-element arrays, and so on until you end up with the initial array sorted.
+
+
 def mergeSort(arr):
     if len(arr) > 1:
 
