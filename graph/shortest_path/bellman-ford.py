b3ae489d677fb83f9133443605cf773162dbba4a
tatitati <francis.albusac@simplybusiness.co.uk>
2021-05-03 20:08:18 +0100

    bellman ford

--- graph/shortest_path/bellman-ford.py
+++ graph/shortest_path/bellman-ford.py
@@ -1,39 +1,36 @@
 INF = float("inf")
 
 def bellman_ford(graph, nodes, src):
-    dist = {node: INF for node in nodes}
-    dist[src] = 0
-    iterations = len(nodes) - 1
+    dists = {node: INF for node in nodes}
+    dists[src] = 0
+    # table -> {'a': 0, 'b': inf, 'c': inf, 'd': inf, 'e': inf}
 
-    # iterations
-    for _ in range(iterations):
-        for s, d, w in graph:
-            if dist[s] != INF and dist[s] + w < dist[d]:
-                dist[d] = dist[s] + w
 
-    # Step 3: detect negative cycle
-    # if value changes then we have a negative cycle in the graph and we cannot find the shortest distances
-    for s, d, w in graph:
-        if dist[s] != INF and dist[s] + w < dist[d]:
-            print("Graph contains negative weight cycle")
-            return
+    # "Relax" iterations. Each node is processed N-1 times
+    for _ in range(len(nodes) - 1):
+        for node in graph:
+            for neighboor, distance in graph[node].items():
+                if dists[node] + distance < dists[neighboor]:
+                    dists[neighboor] = dists[node] + distance
 
-    # No negative weight cycle found
-    return dist
+    # check negative cycles
+    for node in graph:
+        for neighboor, distance in graph[node].items():
+            if dists[node] + distance < dists[neighboor]:
+                print("Graph contains negative weight cycle")
+                return
+
+    return dists
 
 
 nodes = ["a", "b", "c", "d", "e"]
-graph = [
-    # s, d, w
-    ["a", "b", -1],
-    ["a", "c", 4],
-    ["b", "c", 3],
-    ["b", "d", 2],
-    ["b", "e", 2],
-    ["d", "c", 5],
-    ["d", "b", 1],
-    ["e", "d", -3]
-]
+graph = {
+    'a': {'b': -1, 'c': 4},
+    'b': {'c': 3, 'd': 2, 'e': 2},
+    'd': {'c': 5, 'b': 1},
+    'e': {'d': -3}
+}
+
 
 print(bellman_ford(graph, nodes, "a"))
 # {
