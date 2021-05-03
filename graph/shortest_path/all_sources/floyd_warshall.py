0f510c8152b58bc9647ce5d88b5d95cc5325e9ca
tatitati <francis.albusac@simplybusiness.co.uk>
2021-05-03 20:20:44 +0100

    more

--- graph/shortest_path/all_sources/floyd_warshall.py
+++ graph/shortest_path/all_sources/floyd_warshall.py
@@ -17,8 +17,18 @@ def floydWarshall(graph,n): #n=amount of vertex
                 )
     return dists
 
+#          10
+#    (0)------->(3)
+#     |         /|\
+#   5 |          |
+#     |          | 1
+#    \|/         |
+#    (1)------->(2)
+#         3
 
+# adjacency matrix
 graph = [
+    # 0, 1, 2, 3
     [0, 5, INF, 10],
     [INF, 0, 3, INF],
     [INF, INF, 0, 1],
