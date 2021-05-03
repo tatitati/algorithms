303ee0c18a193bff0d83248075cb7af4600f7c0d
tatitati <francis.albusac@simplybusiness.co.uk>
2021-05-03 20:46:13 +0100

    more

--- Sort/BubbleSort.py
+++ Sort/BubbleSort.py
@@ -1,3 +1,8 @@
+# Bubble sort is a simple sorting algorithm. This sorting algorithm is comparison-based algorithm in
+# which each pair of adjacent elements is compared and the elements are swapped if they are not in order.
+# This algorithm is not suitable for large data sets as its average and worst case
+# complexity are of Ο(n2) where n is the number of items.
+
 def bubbleSort(nums):
     madeChanges = None
     while(madeChanges != False):
