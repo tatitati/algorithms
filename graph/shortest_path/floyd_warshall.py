a5aeee513d9eeb327ef89b1b22e54e76e37e6e6b
tatitati <francis.albusac@simplybusiness.co.uk>
2021-05-03 15:19:32 +0100

    floyd warshall

---
+++ graph/shortest_path/floyd_warshall.py
@@ -0,0 +1,27 @@
+INF = 99999
+def floydWarshall(graph,n): #n=no. of vertex
+    dist=graph
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                dist[i][j] = min(
+                    dist[i][j],
+                    dist[i][k] + dist[k][j]
+                )
+    return dist
+
+
+graph = [
+    [0, 5, INF, 10],
+    [INF, 0, 3, INF],
+    [INF, INF, 0, 1],
+    [INF, INF, INF, 0]
+]
+
+print(floydWarshall(graph, 4))
+# [
+#   [0, 5, 8, 9],
+#   [99999, 0, 3, 4],
+#   [99999, 99999, 0, 1],
+#   [99999, 99999, 99999, 0]
+# ]
