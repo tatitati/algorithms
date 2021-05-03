494088d07a810b9452b3ffd547c46e8e944a1b8a
tatitati <francis.albusac@simplybusiness.co.uk>
2021-05-03 16:22:36 +0100

    bubble

--- Sort/BubbleSort.py
+++ Sort/BubbleSort.py
@@ -1,12 +1,16 @@
-def bubbleSort(arr):
-    n = len(arr)
-    for i in range(n-1):
-        for j in range(0, n-i-1):
-            if arr[j] > arr[j+1] :
-                arr[j], arr[j+1] = arr[j+1], arr[j]
+def bubbleSort(nums):
+    madeChanges = None
+    while(madeChanges != False):
+        madeChanges = False
 
-    return arr
+        for idx, num in enumerate(nums):
+            if idx == len(nums)-1: continue
+            elif nums[idx] > nums[idx+1]:
+                nums[idx], nums[idx+1] = nums[idx+1], nums[idx] # swap
+                madeChanges = True
+    return nums
 
-arr = [64, 34, 25, 12, 22, 11, 90]
 
-print(bubbleSort(arr)) # [11, 12, 22, 25, 34, 64, 90]
+nums = [64, 34, 25, 12, 22, 11, 90]
+
+print(bubbleSort(nums)) # [11, 12, 22, 25, 34, 64, 90]
