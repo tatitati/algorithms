f5af625ad5d48b48d1340a3f8e23091e8472a149
tatitati <francis.albusac@simplybusiness.co.uk>
2021-05-07 00:12:28 +0100

    more

--- Sort/InsertionSort.py
+++ Sort/InsertionSort.py
@@ -1,7 +1,9 @@
-# An insertion sort compares values in turn, starting with the second value in the list. If this value is greater
-# than the value to the left of it, no changes are made. Otherwise this value is repeatedly moved left until it meets a
-# value that is less than it. The sort process then starts again with the next value. This continues until the end of
-# the list is reached.
+"""
+An insertion sort compares values in turn, starting with the second value in the list. If this value is greater
+than the value to the left of it, no changes are made. Otherwise this value is repeatedly moved left until it meets a
+value that is less than it. The sort process then starts again with the next value. This continues until the end of
+the list is reached.
+"""
 
 def insertion_sort(nums):
     for i, value in enumerate(nums):
