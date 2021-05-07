94c8b50f676abec6448efdd4a891e2adddb83bfe
tatitati <francis.albusac@simplybusiness.co.uk>
2021-05-07 23:24:31 +0100

    more

--- dynamic_programming/fibonacci.py
+++ dynamic_programming/fibonacci.py
@@ -1,3 +1,4 @@
+# BOTTOM-UP (tabulation) approach
 def fib(n):
     fibs = []
     for i in range(n+1):
@@ -8,5 +9,19 @@ def fib(n):
 
     print(fibs) # [0, 1, 1, 2, 3]
     return fibs.pop() # last item
+
+print(fib(4)) # 3
+
+
+# TOP-BOTTOM (memoization) approach
+acc = []
+def fib(n):
+    if n < 0: return
+    if n == 0 or n == 1:
+        acc.insert(n, n)
+    else:
+        acc.insert(n, fib(n-2) + fib(n-1))
+
+    return acc[n]
 
 print(fib(4)) # 3
