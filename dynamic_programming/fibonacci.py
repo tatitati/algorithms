b170dbfce47d9d3062655abc5ab5d06356efd808
tatitati <francis.albusac@simplybusiness.co.uk>
2021-05-07 22:28:01 +0100

    added fibonacci with Dynamic Programming

---
+++ dynamic_programming/fibonacci.py
@@ -0,0 +1,13 @@
+
+def fib(n):
+    fibs = []
+    for i in range(n+1):
+        if i == 0 or i == 1:
+            fibs.insert(i, i)
+        else:
+            fibs.insert(i, fibs[i-1] + fibs[i-2])
+
+    print(fibs) # [0, 1, 1, 2, 3]
+    return fibs.pop() # last item
+
+print(fib(4)) # 3
