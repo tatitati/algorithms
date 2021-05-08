9f16a92da73879f943fbeddd8c99fa65b0f98570
tatitati <francis.albusac@simplybusiness.co.uk>
2021-05-08 16:55:39 +0100

    more

--- data_structures/string/countConsonants.py
+++ data_structures/string/countConsonants.py
@@ -3,9 +3,23 @@ def countConsonantsIterative(text):
     for i in text:
         if i not in ['A', 'E', 'I', 'O', 'U'] and i not in ['a', 'e', 'i', 'o', 'u']:
             acc += 1
-
     return acc
 
+print(countConsonantsIterative(list("hello"))) # 3
 
 
-print(countConsonantsIterative(list("hello")))
+
+
+
+
+def countConsonantsRecursive(text):
+    print(text)
+    if len(text) <= 1:    
+        return 1 if text[0] not in ['A', 'E', 'I', 'O', 'U'] and text[0] not in ['a', 'e', 'i', 'o', 'u'] else 0    
+
+    if text[0] not in ['A', 'E', 'I', 'O', 'U'] and text[0] not in ['a', 'e', 'i', 'o', 'u']:
+        return  1 + countConsonantsRecursive(text[1:])
+    else:
+        return  0 + countConsonantsRecursive(text[1:])
+    
+print(countConsonantsRecursive(list("hello"))) # 3
