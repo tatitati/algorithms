5fd0d25a443dcf2fb71496dff457daef2a8289b1
tatitati <francis.albusac@simplybusiness.co.uk>
2021-05-09 14:49:53 +0100

    more

--- dynamic_programming/max_array_sum.py
+++ dynamic_programming/max_array_sum.py
@@ -1,4 +1,21 @@
+'''
+Given an array of integers, find the subset of non-adjacent elements with the maximum sum. 
+Calculate the sum of that subset. It is possible that the maximum sum is, the case when 
+all elements are negative.
 
+Example
+The following subsets with more than
+element exist. These exclude the empty subset and single element subsets which are also valid.
+
+Subset      Sum
+[-2, 3, 5]   6
+[-2, 3]      1
+[-2, -4]    -6
+[-2, 5]      3
+[1, -4]     -3
+[1, 5]       6
+[3, 5]       8
+'''
 
 # nums = [3, 7, 4, 6, 5] 
 # Expected 13
