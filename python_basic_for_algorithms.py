d81a15b250d74f69839ee59d92e44c826261170b
tatitati <francis.albusac@simplybusiness.co.uk>
2021-05-09 22:01:06 +0100

    more

--- python_basic_for_algorithms.py
+++ python_basic_for_algorithms.py
@@ -18,7 +18,8 @@ print(a)
 
 # range
 # =====
-print(list(range(5)))  # [0, 1, 2, 3, 4]
+print(list(range(5)))  	  # [0, 1, 2, 3, 4]
+print(list(range(1, 5)))  # [1, 2, 3, 4]
 
 
 # for + len + range VS for + enumerate
