4d79a221760ff073ef9ce3d71442029da7510eb4
tatitati <francis.albusac@simplybusiness.co.uk>
2021-05-09 21:41:09 +0100

    some notes of python

--- python_basic_for_algorithms.py
+++ python_basic_for_algorithms.py
@@ -1,3 +1,6 @@
+'''
+common pieces of code across all the algorithms in python
+'''
 
 print(list(range(5)))  # [0, 1, 2, 3, 4]
 
@@ -9,3 +12,15 @@ for idx in range(len(["a", "b", "c", "d", "e"])):
 for idx, _ in enumerate(["a", "b", "c", "d", "e"]):
 	print(idx) # 0 1 2 3 4 
 
+
+a = ["a", "b", "c", "d", "e"]
+# up to...
+print(a[:0]) # []
+print(a[:1]) # a
+print(a[:-1]) # ['a', 'b', 'c', 'd']
+# from...
+print(a[2:]) # c d e
+# between...
+print(a[1:-1]) # b c d
+# between jumping...
+print(a[1:-1:2]) # b d
