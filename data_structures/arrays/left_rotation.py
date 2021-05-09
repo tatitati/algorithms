cebe64145c8378b3449e3720afb3a0f4e532459a
tatitati <francis.albusac@simplybusiness.co.uk>
2021-05-09 22:07:05 +0100

    more

--- data_structures/arrays/left_rotation.py
+++ data_structures/arrays/left_rotation.py
@@ -1,7 +1,9 @@
-def rotLeft(listNumbers, rotations):
-    for x in range(rotations):
-        listNumbers = listNumbers[1:] + listNumbers[:1]
-    return listNumbers
+def rotLeft(numbers, rotations):
+    result = [0]*len(numbers)
+    for idx, val in enumerate(numbers):
+        result[idx-rotations] = val
+        
+    return result
 
 
 print(rotLeft([1,2,3,4,5,6,7,8], 5)) # [6, 7, 8, 1, 2, 3, 4, 5]
