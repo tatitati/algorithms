415e1a266ba3500f6e7fec6936b094e7c02bfaa0
tatitati <francis.albusac@simplybusiness.co.uk>
2021-05-09 14:40:58 +0100

    more

--- dynamic_programming/coinchange.py
+++ dynamic_programming/coinchange.py
@@ -12,10 +12,16 @@ So output should be 4.
 *For N = 10 and S = {2, 5, 3, 6}, there are five solutions: {2,2,2,2,2}, {2,2,3,3}, {2,2,6}, {2,3,5} and {5,5}. 
 So the output should be 5.
 '''
-def coinChange( T, L ):
+def coinChange( coins, amount ):
+	Opt = [0 for i in range(0, amount+1)]
+	n = len(coins)
 
+	for i in range(1, amount+1):
+		smallest = float("inf")
+		for j in range(0, n):
+			if (coins[j] <= i): 
+				smallest = min(smallest, Opt[i - coins[j]]) 
+		Opt[i] = 1 + smallest 
+	return Opt[amount]
 
-
-
-
-print(coinChange([[1, 3, 4],  20]))
+print(coinChange([1, 2, 3],  4))
