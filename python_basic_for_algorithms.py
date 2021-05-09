73b312bca865097796fe6ca0bb64eba3d6427cd3
tatitati <francis.albusac@simplybusiness.co.uk>
2021-05-09 21:55:47 +0100

    more

--- python_basic_for_algorithms.py
+++ python_basic_for_algorithms.py
@@ -18,8 +18,8 @@ print(a)
 print(list(range(5)))  # [0, 1, 2, 3, 4]
 
 
-# for + len + range
-# =================
+# for + len + range VS for + enumerate
+# ====================================
 # you will see this loop a lot. Trick: is much easier to read in the second way (with enumerate)
 for idx in range(len(["a", "b", "c", "d", "e"])):
 	print(idx) # 0 1 2 3 4 
