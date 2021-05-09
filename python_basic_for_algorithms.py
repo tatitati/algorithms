0ee781057a8af5e92ad8ee79b9ebf1ff6d0d60a7
tatitati <francis.albusac@simplybusiness.co.uk>
2021-05-09 21:43:17 +0100

    more

--- python_basic_for_algorithms.py
+++ python_basic_for_algorithms.py
@@ -2,9 +2,17 @@
 common pieces of code across all the algorithms in python
 '''
 
+# infinity
+# ========
+print(float("inf")) # inf
+
+# range
+# =====
 print(list(range(5)))  # [0, 1, 2, 3, 4]
 
 
+# for + len + range
+# =================
 # you will see this loop a lot. Trick: is much easier to read in the second way (with enumerate)
 for idx in range(len(["a", "b", "c", "d", "e"])):
 	print(idx) # 0 1 2 3 4 
@@ -12,7 +20,8 @@ for idx in range(len(["a", "b", "c", "d", "e"])):
 for idx, _ in enumerate(["a", "b", "c", "d", "e"]):
 	print(idx) # 0 1 2 3 4 
 
-
+# array slices
+# ============
 a = ["a", "b", "c", "d", "e"]
 # up to...
 print(a[:0]) # []
@@ -24,3 +33,7 @@ print(a[2:]) # c d e
 print(a[1:-1]) # b c d
 # between jumping...
 print(a[1:-1:2]) # b d
+
+
+# init matrixes
+# =============
