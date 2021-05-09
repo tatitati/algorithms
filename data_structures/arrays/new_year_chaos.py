a4aaa2d11b36ae4032c7c9b13ba9dd88bfa63a9d
tatitati <francis.albusac@simplybusiness.co.uk>
2021-05-10 00:38:01 +0100

    more

--- data_structures/arrays/new_year_chaos.py
+++ data_structures/arrays/new_year_chaos.py
@@ -1,21 +1,22 @@
 
 def bribe(q):
-	origQ = range(1, len(q)+1)
-
 	bribe = 0
-	for idx, val in enumerate(q):
-		if val - origQ[idx] > 2:
-			print("too chaotic")
+	memoBribers = [0]*len(q)
+	for idx, val in enumerate(q):				
+		if val - (idx+1) > 2:
+			print("Too chaotic")
 			return
-		else:			
-			bribe += val - origQ[idx]
+		
+		for j in q[:idx]:
+			if j > val:
+				bribe += 1 
 
 	print(bribe)
 
 
 
-# print(bribe([4, 1, 2, 3])) # too chaotic
-# bribe([1, 2, 3, 5, 4, 6, 7, 8]) # 1
-# bribe([2, 1, 5, 3, 4]) # 3 --> 5 bribed twice, and 2 bribed one
-# bribe([2, 5, 1, 3, 4]) # too chaotic
-bribe([1, 2, 5, 3, 7, 8, 6, 4]) # 
+bribe([4, 1, 2, 3]) # too chaotic
+bribe([1, 2, 3, 5, 4, 6, 7, 8]) # 1
+bribe([2, 1, 5, 3, 4]) # 3 --> 5 bribed twice, and 2 bribed one
+bribe([2, 5, 1, 3, 4]) # too chaotic
+bribe([1, 2, 5, 3, 7, 8, 6, 4]) # 7
