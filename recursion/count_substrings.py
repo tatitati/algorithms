736f5b864b0955f2318e0dbc1e6ce42b180a5e03
tatitati <francis.albusac@simplybusiness.co.uk>
2021-05-20 23:51:10 +0100

    more

--- recursion/count_substrings.py
+++ recursion/count_substrings.py
@@ -1,17 +1,24 @@
 # https://www.geeksforgeeks.org/recursive-solution-count-substrings-first-last-characters/
 
-def countSubstrings(text, searchLetter):	
+def recursiveSearch(text, searchLetter):	
+	resultIteration = 1 if text[0] == searchLetter else 0	
+
 	# base case
 	if len(text) == 1:
-		return 1 if text[0] == searchLetter else 0
-
-	return 1 + text[]
-
-
+		return resultIteration
+	
+	return resultIteration + recursiveSearch(text[1:], searchLetter)
 
 
+def main(text):
+	result = 0
+	for i, _ in enumerate(text):
+		result += recursiveSearch(text[i:], text[i])
+
+	return result
 
 
-countSubstrings(["a", "b", "a"]) # 4
-countSubstrings(["a", "b", "c", "a", "b"]) # 7
-countSubstrings(["a", "b", "c", "a", "b", "a", "c"]) # 12
+
+print(main(["a", "b", "a"])) # 4
+print(main(["a", "b", "c", "a", "b"])) # 7
+print(main(["a", "b", "c", "a", "b", "a", "c"])) # 12
