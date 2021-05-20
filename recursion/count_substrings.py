182cf3a6d310f6b6975c985a900e3c7f5d291cd5
tatitati <francis.albusac@simplybusiness.co.uk>
2021-05-20 23:27:52 +0100

    more

--- recursion/count_substrings.py
+++ recursion/count_substrings.py
@@ -1,8 +1,12 @@
 # https://www.geeksforgeeks.org/recursive-solution-count-substrings-first-last-characters/
 
-def countSubstrings(text):
+def countSubstrings(text, searchLetter):	
+	
 
 
 
 
-countSubstrings("aba") # 4
+
+countSubstrings(["a", "b", "a"]) # 4
+countSubstrings(["a", "b", "c", "a", "b"]) # 7
+countSubstrings(["a", "b", "c", "a", "b", "a", "c"]) # 12
