0e5dc9af66c0fa8fdbb5e07b485a285169d6df23
tatitati <francis.albusac@simplybusiness.co.uk>
2021-05-22 17:34:32 +0100

    more

--- recursion/backtracking/rat_in_laberinth.py
+++ recursion/backtracking/rat_in_laberinth.py
@@ -1,41 +1,56 @@
 # https://www.geeksforgeeks.org/rat-in-a-maze-backtracking-2/
+import time
+
+currentSolutionPath = [
+		[0, 0, 0, 0],
+		[0, 0, 0, 0],
+		[0, 0, 0, 0],
+		[0, 0, 0, 0]]
 
 def solve(maze, start, goal, position):
+	currentSolutionPath[position[0]][position[1]] = 1
 	print([position[0], position[1]])
 
 	# current step
 	# - is goal?
 	if position == goal: 
-		print("GOAL")
+		print("GOAL")	
 		return True
+
+	# - rat is outside maze?
+	if position[0] > len(maze[0]) - 1 or position[0] < 0: 
+		print("OUT: Y")
+		return False
+
+	if position[1] > len(maze) - 1 or position[1] < 0: 
+		print("OUT: X")
+		return False
+
 	# - rat is in a wall?
 	if maze[position[0]][position[1]] == 0: 
 		print("WALL")
 		return False
-	# - rat is outside maze?
-	if position[0] > len(maze[0]) - 1 or position[1] > len(maze) - 1: 
-		print("OUT")
-		return False
-
 
 	# next step:
-	# try right
-	print("right?")
-	if solve(maze, start, goal, [position[0]+1, position[1]]) == False:
-		print("left?")
-		# try left
-		if solve(maze, start, goal, [position[0]-1, position[1]]) == False:
-			print("top?")
-			# try top
-			if solve(maze, start, goal, [position[0], position[1]-1]) == False:
-				print("bottom?")
-				# try bottom
-				if solve(maze, start, goal, [position[0], position[1]+1]) == False:
+
+	print("\nright?")
+	time.sleep(1)
+	if currentSolutionPath[position[0]][position[1]+1] == 1 or solve(maze, start, goal, [position[0], position[1]+1]) == False: 	# try right
+		print("\nleft?")
+		time.sleep(1)
+		if currentSolutionPath[position[0]][position[1]-1] == 1 or solve(maze, start, goal, [position[0], position[1]-1]) == False: 	# try left
+			print("\ntop?")
+			time.sleep(1)
+			if currentSolutionPath[position[0]-1][position[1]] == 1 or solve(maze, start, goal, [position[0]-1, position[1]]) == False: # try top
+				print("\nbottom?")
+				time.sleep(1)				
+				if currentSolutionPath[position[0]+1][position[1]] == 1 or solve(maze, start, goal, [position[0]+1, position[1]]) == False: # try bottom
 					return False
 
-	# try right	
-	print("end")
-	return solve(maze, start, goal, [position[0]+1, position[1]])
+	
+	# # try right	
+	# print("end")
+	# return solve(maze, start, goal, [position[0]+1, position[1]])
 
 
 
@@ -47,4 +62,4 @@ maze = [[1, 0, 0, 0],
 		[0, 1, 0, 0],
 		[1, 1, 1, 1]]
 
-print(solve(maze, start, end, start))
+solve(maze, start, end, start)
