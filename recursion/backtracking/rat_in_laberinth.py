6267ec036608b342a68c3b0adda2c3fb189e1fed
tatitati <francis.albusac@simplybusiness.co.uk>
2021-05-22 16:38:51 +0100

    maze

--- recursion/backtracking/rat_in_laberinth.py
+++ recursion/backtracking/rat_in_laberinth.py
@@ -1,30 +1,40 @@
 # https://www.geeksforgeeks.org/rat-in-a-maze-backtracking-2/
 
-
-
 def solve(maze, start, goal, position):
-	if position == goal: return True
-	
-	# rat is in a wall
-	if maze[position[0]][position[1]] == 0: return False
-	# rat is outside maze
-	if position[0] > len(maze[0]) - 1 or position[1] > len(maze) - 1: return False
+	print([position[0], position[1]])
 
+	# current step
+	# - is goal?
+	if position == goal: 
+		print("GOAL")
+		return True
+	# - rat is in a wall?
+	if maze[position[0]][position[1]] == 0: 
+		print("WALL")
+		return False
+	# - rat is outside maze?
+	if position[0] > len(maze[0]) - 1 or position[1] > len(maze) - 1: 
+		print("OUT")
+		return False
+
+
+	# next step:
 	# try right
+	print("right?")
 	if solve(maze, start, goal, [position[0]+1, position[1]]) == False:
-
+		print("left?")
 		# try left
 		if solve(maze, start, goal, [position[0]-1, position[1]]) == False:
-
+			print("top?")
 			# try top
 			if solve(maze, start, goal, [position[0], position[1]-1]) == False:
-
+				print("bottom?")
 				# try bottom
-				if solve(maze, start, goal, [position[0]+1, position[1]+1]) == False:
+				if solve(maze, start, goal, [position[0], position[1]+1]) == False:
 					return False
 
-	# try right
-	print([position[0]+1, position[1]])
+	# try right	
+	print("end")
 	return solve(maze, start, goal, [position[0]+1, position[1]])
 
 
