45b5fad3fa3732d72e66097aca740f640628e235
tatitati <francis.albusac@simplybusiness.co.uk>
2021-06-05 12:56:48 +0100

    more

--- Search/binary_search.py
+++ Search/binary_search.py
@@ -13,18 +13,25 @@ def binary_search_iterative(arr, x):
 
     return -1
 
-def binary_search_recursive(arr, low, high, x):
-    if high >= low:
-        mid = (high + low) // 2
 
-        if arr[mid] == x: return mid # base condition
-        elif arr[mid] > x: return binary_search_recursive(arr, low, mid - 1, x)
-        else: return binary_search_recursive(arr, mid + 1, high, x)
+def binary_search_recursive(arr, target, lowIdx = None, highIdx = None):
+	# 1. define base case
+	if lowIdx != None and highIdx != None and lowIdx == highIdx: 
+		if arr[lowIdx] == target: return lowIdx
+		else: return -1
 
-    else: return -1
+	# 2. define next recursion
+	if lowIdx == None or highIdx == None:		
+		highIdx = len(arr) - 1
+		lowIdx = 0
 
+	mid = (lowIdx + highIdx) // 2
+	if target == arr[mid]: return mid
+	if target <  arr[mid]: return binarysearch1(arr, target, lowIdx, mid-1) # search to the right
+	elif target > arr[mid]: return binarysearch1(arr, target, mid+1, highIdx) # search to the right
+	
 
 arr = [ 2, 3, 4, 8, 10, 30, 40, 60 ]
 print(binary_search_iterative(arr, 4)) # element present at index: 3
-print(binary_search_recursive(arr, 0, len(arr)-1, 4)) # element present at index: 3
+print(binary_search_recursive(arr, 0)) # element present at index: 3
 
