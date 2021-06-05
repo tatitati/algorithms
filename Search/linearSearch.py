d9ba9c94a1025d19761aad209eee42a6e521e99f
tatitati <francis.albusac@simplybusiness.co.uk>
2021-06-05 11:05:49 +0100

    el linear search de los croyons

--- Search/linearSearch.py
+++ Search/linearSearch.py
@@ -1,7 +1,14 @@
 
 # Complexity
 # ===========
-# Worst case time complexity: O(N)
 # Average case time complexity: O(N)
-# Best case time complexity: O(1)
+# Best case (you find it at the start) time complexity: O(1)
+# Worst case (you find it at the end) time complexity: O(N)
 # Space complexity: O(1)
+
+
+def linearSearch(words, target):
+	for i, word in enumerate(words):
+		if word == target: return i
+
+linearSearch(["tree", "car", "castle", "ocean", "universe", "hello", "something"], "hello")
